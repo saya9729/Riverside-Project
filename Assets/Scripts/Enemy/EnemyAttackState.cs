cd71faff4f006071bc2627f8757b13a11a84ef3f
saya9729 <bmdtai18@apcs.fitus.edu.vn>
2022-03-30 10:59:51 +0700

    New attack logic

--- Assets/Scripts/Enemy/EnemyAttackState.cs
+++ Assets/Scripts/Enemy/EnemyAttackState.cs
@@ -6,6 +6,7 @@ namespace Enemy
     public class EnemyAttackState : AbstractClass.State
     {
         private EnemyStateManager _enemyStateManager;
+        [SerializeField] private float attackRange;
 
         private void Start()
         {
@@ -13,18 +14,25 @@ namespace Enemy
         }
         public override void EnterState()
         {
-            Debug.Log("Enter Attack State");
-            //start attack animation
+            Debug.Log("Enter Attack State");            
         }
 
         public override void UpdateState()
         {
-
+            if (Vector3.Distance(transform.position, _enemyStateManager.player.transform.position) < attackRange)
+            {
+                //start attack animation
+            }
+            else
+            {
+                _enemyStateManager.SwitchState(_enemyStateManager.enemyPatrolState);
+            }
         }
 
         public override void ExitState()
         {
             Debug.Log("Exit Attack State");
+            //cancel attack animation
         }
         public override void PhysicsUpdateState()
         {

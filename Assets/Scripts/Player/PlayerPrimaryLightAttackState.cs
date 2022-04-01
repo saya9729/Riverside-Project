53f025ff1272f43992a5b40b239966b3abc52814
letuankhanhAPCS <ltkhanh18@apcs.vn>
2022-04-01 18:50:36 +0700

    Add basic implemetation of primary and secondary attack

--- Assets/Scripts/Player/PlayerPrimaryLightAttackState.cs
+++ Assets/Scripts/Player/PlayerPrimaryLightAttackState.cs
@@ -6,15 +6,31 @@ namespace Player
     public class PlayerPrimaryLightAttackState : AbstractClass.State
     {
         private PlayerStateManager _playerStateManager;
+        private Animator _anim;
+
+        [Header("Primary Attack (Slash)")]
+        //[SerializeField] private float range;
+        [SerializeField] private float maxDamage = 10;
+        [SerializeField] private float attackInterval = 0.5f;
+        //[SerializeField] private float reloadTime;
+        //[SerializeField] private float ammo;
+
+        public void PrimaryAttack()
+        {
+            _anim.SetInteger("attack", 1);
+            //toggle weapon collider
+        }
 
         private void Start()
         {
             _playerStateManager = GameObject.Find("PlayerStateManager").GetComponent<PlayerStateManager>();
+            _anim = gameObject.transform.parent.GetComponent<Animator>();
         }
 
         public override void EnterState()
         {
             Debug.Log("enter primary light attack state");
+            PrimaryAttack();
 
         }
 
@@ -26,6 +42,7 @@ namespace Player
         public override void ExitState()
         {
             Debug.Log("exit primary light attack state");
+            _anim.SetInteger("attack", 0); //return to idle
 
         }
         public override void PhysicsUpdateState()
--- Assets/Scripts/Player/PlayerSecondaryAttackState.cs
+++ Assets/Scripts/Player/PlayerSecondaryAttackState.cs
@@ -6,15 +6,54 @@ namespace Player
     public class PlayerSecondaryAttackState : AbstractClass.State
     {
         private PlayerStateManager _playerStateManager;
+        private Animator _anim;
+
+        [Header("Secondary Attack (Shot)")]
+        [SerializeField] private float range = 4;
+        [SerializeField] private float maxDamage = 20;
+        //[SerializeField] private float attackInterval; 
+        [SerializeField] private float reloadTime = 1;
+        [SerializeField] private float ammo = 2;
+        [SerializeField] private float velocity = 10;
+        [SerializeField] private float delay = 1.2f;
+
+        public GameObject bullet;
+        public Transform nozzlePoint;
+
+        private bool hasShot = false;
+
+        public void SecondaryAttack()
+        {
+            _anim.SetInteger("attack", 2);
+            StartCoroutine(WaitAnim(delay));
+        }
+
+        IEnumerator WaitAnim(float time) //wait animation ready to shoot
+        {
+            yield return new WaitForSeconds(time);
+
+            var shot = Instantiate(bullet, nozzlePoint.position, Quaternion.identity);
+            shot.GetComponent<Rigidbody>().velocity = nozzlePoint.right * velocity;
+        }
 
         private void Start()
         {
-            _playerStateManager = GameObject.Find("PlayerStateManager").GetComponent<PlayerStateManager>();
+            _playerStateManager = GameObject.Find("PlayerStateManager").GetComponent<PlayerStateManager>(); //create bullet
+            _anim = gameObject.transform.parent.GetComponent<Animator>(); //set velocity to bullet
         }
+
+        // private void Update() {
+        //     if(_anim.GetInteger("attack") == 2 && !hasShot)
+        //     {
+        //         StartCoroutine(WaitAnim(delay));
+        //         hasShot = true;
+        //     }
+        // }
 
         public override void EnterState()
         {
             Debug.Log("enter secondary attack state");
+            SecondaryAttack();
 
         }
 
@@ -26,6 +65,8 @@ namespace Player
         public override void ExitState()
         {
             Debug.Log("exit secondary attack state");
+            _anim.SetInteger("attack", 0);
+            StopAllCoroutines(); // stop all shooting sequences
 
         }
         public override void PhysicsUpdateState()

fad3a94f799d6ed4e519a71069d6b34f11454e46
nhvtdoox <nhockcon21@gmail.com>
2022-06-07 18:57:17 +0700

    Revert "update menu"
    This reverts commit e75d8980bb64f5ff60fc64b46b19c9fb516496d6.

--- Assets/_Scripts/UIController/Menu/MainMenu.cs
+++ Assets/_Scripts/UIController/Menu/MainMenu.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 namespace GameUI
@@ -18,31 +17,7 @@ namespace GameUI
             Cursor.lockState = CursorLockMode.Locked;
         }
 
-        [Header("Levels to Load")]
-        public string _newGameLevel;
-        private string _levelToLoad;
-        [SerializeField] private GameObject noSavedGameDialog = null;
-
-        public void NewGameDialogYes()
-        {
-            SceneManager.LoadScene(_newGameLevel);
-        }
-
-        public void LoadGameDialogYes()
-        {
-            if (PlayerPrefs.HasKey("SavedLevel"))
-            {
-                _levelToLoad = PlayerPrefs.GetString("SavedLevel");
-                SceneManager.LoadScene(_levelToLoad);
-            }
-            else
-            {
-                noSavedGameDialog.SetActive(true);
-            }
-                
-        }
-
-        public void ExitButton()
+        public void QuitGame()
         {
             Debug.Log("Quit");
             Application.Quit();

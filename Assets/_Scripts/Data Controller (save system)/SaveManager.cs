d406809d4c50bbdfe51a1e1acc14fc1847607e3c
nhvtdoox <50902539+nhvtdoox@users.noreply.github.com>
2022-06-17 15:21:35 +0700

    [RP-662][RP-661][Publish the MVP on Itch.io][Publish the MVP on Unity Play] (#91)
    * Update/Optimize and shipping
    
    * New/Logo and icon added
    
    * New/scene instruction, lower volume

--- Assets/_Scripts/Data Controller (save system)/SaveManager.cs
+++ Assets/_Scripts/Data Controller (save system)/SaveManager.cs
@@ -6,15 +6,14 @@ public static class SaveManager
 {
     private static string _saveFileName = "/save.sa";
     private static string _savePath = Application.persistentDataPath + _saveFileName;
-    
+    private static BinaryFormatter  _formatter = new BinaryFormatter();
+
     public static void SavePlayer(Player.PlayerStatisticManager p_playerStatisticManager)
     {
-        BinaryFormatter formatter = new BinaryFormatter();
-
         FileStream fileStream = new FileStream(_savePath, FileMode.Create);
         PlayerData playerData = new PlayerData(p_playerStatisticManager);
 
-        formatter.Serialize(fileStream, playerData);
+        _formatter.Serialize(fileStream, playerData);
         fileStream.Close();
 
         Debug.Log("Save " + _savePath);
@@ -22,19 +21,44 @@ public static class SaveManager
 
     public static PlayerData LoadPlayer()
     {
-        if (!File.Exists(_savePath)) 
+        if (!FileSaveExist()) 
         {
             Debug.LogWarning("save file not found: " + _savePath );
             return null;
         }
-        BinaryFormatter formatter = new BinaryFormatter();
         FileStream fileStream = new FileStream(_savePath, FileMode.Open);
 
-        PlayerData playerData = formatter.Deserialize(fileStream) as PlayerData;
+        PlayerData playerData = _formatter.Deserialize(fileStream) as PlayerData;
 
         fileStream.Close();
         Debug.Log("Load " + _savePath);
         return playerData;
+    }
+
+    public static void DeletePlayer()
+    {
+        if (!FileSaveExist())
+        {
+            Debug.LogWarning("save file not found: " + _savePath);
+        }
+        else
+        {
+            File.Delete(_savePath);
+            // refresh editor view
+#if UNITY_EDITOR
+            UnityEditor.AssetDatabase.Refresh();
+#endif
+        }
+    }
+
+    public static bool FileSaveExist()
+    {
+        if (!File.Exists(_savePath))
+        {
+            Debug.LogWarning("save file not found: " + _savePath);
+            return false;
+        }
+        return true;
     }
 
 }

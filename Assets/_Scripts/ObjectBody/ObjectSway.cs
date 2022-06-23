d31fe2984b9eff4360371fd43d270cb815f2f366
letuankhanhAPCS <ltkhanh18@apcs.vn>
2022-06-23 11:56:34 +0700

    Update sway script part2

--- Assets/_Scripts/ObjectBody/ObjectSway.cs
+++ Assets/_Scripts/ObjectBody/ObjectSway.cs
@@ -1,19 +1,24 @@
 using System;
 using UnityEngine;
+using Footsteps;
 
 namespace Player
 {
     public class ObjectSway : MonoBehaviour
     {
         private InputManager inputManager;
+        private CharacterController player;
 
         [Header("Sway Settings")]
         [SerializeField] private float smooth = 5;
         [SerializeField] private float multiplier = 3;
+        [SerializeField] private float triggerVelocity = 0.5f;
+        [SerializeField] private float jumpOffset = 7;
 
         void Start()
         {
             inputManager = GetComponentInParent<InputManager>();
+            player = GetComponentInParent<CharacterController>();
         }
 
         private void Update()
@@ -21,6 +26,16 @@ namespace Player
             // get mouse input
             float mouseX = inputManager.look.x * multiplier;
             float mouseY = inputManager.look.y * multiplier;
+
+            if(player.velocity.y >= triggerVelocity)
+            {
+                mouseY -= jumpOffset;
+            }
+
+            if(player.velocity.y <= -triggerVelocity)
+            {
+                mouseY += jumpOffset;
+            }
 
             // calculate target rotation
             Quaternion rotationX = Quaternion.AngleAxis(-mouseX, Vector3.up);

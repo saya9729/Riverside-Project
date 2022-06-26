62d29f6d94b9ba01f31eb389e5bfb98717c34d79
saya9729 <bmdtai18@apcs.fitus.edu.vn>
2022-06-26 10:40:20 +0700

    [RP-809][Change player dash mechanic] (#94)
    * Update player movement state machine
    
    * Update dash mechanic
    
    * Update mainplayer prefab

---
+++ Assets/_Scripts/Player/Movement State Machine/PlayerDashWhileGroundedState.cs
@@ -0,0 +1,64 @@
+﻿namespace Player
+{
+    public class PlayerDashWhileGroundedState : AbstractClass.State
+    {
+        private PlayerMovementStateManager _playerMovementController;
+        public override void EnterState()
+        {
+            _playerMovementController.ResetAirborneDirection();
+            _playerMovementController.DisableJump();
+            _playerMovementController.SetDashDirection();
+            _playerMovementController.DisableInput();
+            _playerMovementController.ResetInputDirection();
+        }
+
+        public override void ExitState()
+        {
+            _playerMovementController.EnableJump();
+            _playerMovementController.EnableInput();
+        }
+
+        protected override void PhysicsUpdateThisState()
+        {
+
+        }
+
+        protected override void CheckSwitchState()
+        {
+            if (!_playerMovementController.isInDashState)
+            {
+                _playerMovementController.SetRunSpeed();
+                currentSuperState.currentSuperState.SwitchToState("Run");
+            }
+            else if (!_playerMovementController.isGrounded)
+            {
+                currentSuperState.SwitchToState("Airborne");
+            }
+        }
+
+        protected override void InitializeComponent()
+        {
+            _playerMovementController = GetComponent<PlayerMovementStateManager>();
+        }
+
+        protected override void InitializeState()
+        {
+
+        }
+
+        protected override void UpdateThisState()
+        {
+            CheckSwitchState();
+        }
+
+        public override void SwitchToState(string p_StateType)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void InitializeVariable()
+        {
+
+        }
+    }
+}

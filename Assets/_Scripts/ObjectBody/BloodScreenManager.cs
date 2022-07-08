e5f12d54176367c34c1f056833842906ba82eed9
letuankhanhAPCS <ltkhanh18@apcs.vn>
2022-07-08 15:16:33 +0700

    Add blood hit screen vfx

---
+++ Assets/_Scripts/ObjectBody/BloodScreenManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloodScreenManager : MonoBehaviour
+{
+    //public static BloodScreenManager instance;
+    private bool isPlaying = false;
+
+    public ImageController[] imageController;
+
+    public void Play()
+    {
+        if (isPlaying) return;
+        else StartCoroutine(ToggleChecker());
+
+        if (imageController.Length == 0) return;
+        var index = Random.Range(0, imageController.Length);
+        imageController[index].EnableThis();
+    }
+
+    IEnumerator ToggleChecker()
+    {
+        isPlaying = true;
+
+        yield return new WaitForSeconds(0.5f);
+
+        isPlaying = false;
+    }
+}
